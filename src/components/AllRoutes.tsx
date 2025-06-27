2ebb886040683b883022c6e60e66bbee90b8aed7
mayur wagh <mswagh98@gmail.com>
2025-06-27 22:21:02 +0530

    add: dummy dashboard

--- src/components/AllRoutes.tsx
+++ src/components/AllRoutes.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import AuthPage from "../pages/Auth";
+import EcommerceDashboard from "../pages/Dashboard";
 
 const AllRoutes = () => {
   return (
     <>
       <Routes>
         <Route path="auth" element={<AuthPage />} />
+        <Route path="dashboard" element={<EcommerceDashboard />} />
       </Routes>
     </>
   );
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import makeApiRequest from "../helpers/axiosFunction";
 import { handleApiError } from "../helpers/handleApiError";
 import { BACKEND_URL } from "../utils/urls";
@@ -11,6 +12,7 @@ interface signupFormProps {
 }
 
 const useAuth = () => {
+  const navigate = useNavigate();
   const userAuth = async (payload: signupFormProps) => {
     try {
       const { data } = await makeApiRequest(
@@ -26,6 +28,7 @@ const useAuth = () => {
           title: "User created",
           description: data.message,
         });
+        navigate("/dashboard");
       }
     } catch (error: unknown) {
       handleApiError(error);
