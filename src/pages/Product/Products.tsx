2748520c471bc6ae56b210c82f87e4a85ddbf660
mayur wagh <mswagh98@gmail.com>
2025-07-04 00:37:41 +0530

    add: add product button

--- src/pages/Product/Products.tsx
+++ src/pages/Product/Products.tsx
@@ -1,8 +1,10 @@
 import SingleProduct from "../../components/Products/SingleProduct";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import { Button } from "../../components/ui/Button";
 import useGetProducts from "../../hooks/product/useGetProducts";
 import useProductStore from "../../store/slices/products";
 import type { Product } from "../../store/types";
+import { IoIosAddCircleOutline } from "react-icons/io";
 
 const Products = () => {
   useGetProducts();
@@ -13,9 +15,15 @@ const Products = () => {
       <Sidebar
         children={
           <div className="max-w-7xl mx-auto px-6 py-12">
-            <h1 className="text-3xl font-bold text-gray-900 mb-8">
-              All Products
-            </h1>
+            <div className="flex justify-between items-center">
+              <h1 className="text-3xl font-bold text-gray-900 mb-8">
+                All Products
+              </h1>
+              <Button className="flex items-center cursor-pointer">
+                <IoIosAddCircleOutline className="mr-1" /> Add Product
+              </Button>
+            </div>
+
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
               {products &&
                 products?.map((product: Product) => (
